3dd4171b4e63397fd2a85a955147dc86d74f5545
jaysonar092 <jaysonar092@gmail.com>
2025-04-19 23:45:39 +0530

    update registration form

--- library/forms.py
+++ library/forms.py
@@ -1,6 +1,8 @@
+from crispy_forms.helper import FormHelper
+from crispy_forms.layout import Submit
 from django import forms
-from django.contrib.auth.models import User
 from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.models import User
 
 class UserRegisterForm(UserCreationForm):
     email = forms.EmailField()
@@ -8,3 +10,14 @@ class UserRegisterForm(UserCreationForm):
     class Meta:
         model = User
         fields = ['username', 'email', 'password1', 'password2']
+        widgets = {
+            'username': forms.TextInput(attrs={'placeholder': 'Choose a username'}),
+            'password1': forms.PasswordInput(attrs={'placeholder': 'Create a password'}),
+            'password2': forms.PasswordInput(attrs={'placeholder': 'Repeat the password'}),
+        }
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.helper = FormHelper()
+        self.helper.form_method = 'post'
+        self.helper.add_input(Submit('submit', 'Register'))
